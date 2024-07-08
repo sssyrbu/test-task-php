ade32899c96aed6dd9ca90467abaaf4a0cd3b1c1
Roman Syrbu <syrburoman0@gmail.com>
2024-07-09 00:20:09 +0300

    Completed task 1-2

--- web/main.php
+++ web/main.php
@@ -13,33 +13,45 @@ while ($id <= $number_of_candles) {
         $candles[] = $candle;
     }
     $id++;
-} 
+}
+
+$candles = array_reverse($candles);
+$data = [];
+
+foreach ($candles as $candle) {
+    $data[] = [
+        'open' => floatval($candle['openPrice']),
+        'high' => floatval($candle['highPrice']),
+        'low' => floatval($candle['lowPrice']),
+        'close' => floatval($candle['closePrice']),
+        'time' => intval($candle['startTime'] / 1000) // Convert milliseconds to seconds
+    ];
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="index.js"></script>
+    <script src="https://unpkg.com/lightweight-charts/dist/lightweight-charts.standalone.production.js"></script>
     <title>BTC candles</title>
 </head>
 <body>
     <h1>BTC candles</h1>
-    <table border="1">
-        <tr>
-            <th>Start Time</th>
-            <th>Open Price</th>
-            <th>High Price</th>
-            <th>Low Price</th>
-            <th>Close Price</th>
-        </tr>
-        <?php foreach ($candles as $candle) { ?>
-        <tr>
-            <td><?php echo $candle['startTime']; ?></td>
-            <td><?php echo $candle['openPrice']; ?></td>
-            <td><?php echo $candle['highPrice']; ?></td>
-            <td><?php echo $candle['lowPrice']; ?></td>
-            <td><?php echo $candle['closePrice']; ?></td>
-        </tr>
-        <?php } ?>
-    </table>
+    <div id="container" style="width: 100%; height: 800px;"></div>
+    <script type="module">
+        document.addEventListener('DOMContentLoaded', () => {
+            console.log("loaded dom");
+            const { createChart } = LightweightCharts;
+
+            const chartOptions = { layout: { textColor: 'white', background: { type: 'solid', color: 'black' } } };
+
+            const chart = createChart(document.getElementById('container'), chartOptions);
+            const candlestickSeries = chart.addCandlestickSeries({ upColor: '#26a69a', downColor: '#ef5350', borderVisible: false, wickUpColor: '#26a69a', wickDownColor: '#ef5350' });
+
+            const data = <?php echo json_encode($data); ?>;
+
+            candlestickSeries.setData(data);
+            chart.timeScale().fitContent();
+        });
+    </script>
 </body>
 </html>
--- web/queries/create_table.sql
+++ web/queries/create_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS btc_candle (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    startTime BIGINT NOT NULL,        
-    openPrice DECIMAL(20, 8) NOT NULL,
-    highPrice DECIMAL(20, 8) NOT NULL,
-    lowPrice DECIMAL(20, 8) NOT NULL,
-    closePrice DECIMAL(20, 8) NOT NULL
+    startTime BIGINT NOT NULL,     
+    openPrice DECIMAL(20, 2) NOT NULL,
+    highPrice DECIMAL(20, 2) NOT NULL,
+    lowPrice DECIMAL(20, 2) NOT NULL,
+    closePrice DECIMAL(20, 2) NOT NULL
 );
