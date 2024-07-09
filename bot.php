90d0b891c3d400a636c3a3c059876c5b5e60e264
Roman Syrbu <syrburoman0@gmail.com>
2024-07-09 19:46:29 +0300

    Removed token

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@ include 'database.php';
 use SergiX44\Nutgram\Nutgram;
 use SergiX44\Nutgram\RunningMode\Polling;
 
-$bot = new Nutgram('6840307916:AAGT4RnFLtF0S-t-_weyAdx14koxU_DQZ3g');
+$bot = new Nutgram('TOKEN');
 $bot->setRunningMode(Polling::class);
 
 $bot->onCommand('start', function(Nutgram $bot) {
